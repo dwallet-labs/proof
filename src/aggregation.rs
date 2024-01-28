ff6211ed69ec273a569895de8042b16b3b564149
Yehonatan Cohen Scaly <scalys7@gmail.com>
2024-01-28 16:07:59 +0200

    doc

--- src/aggregation.rs
+++ src/aggregation.rs
@@ -1,6 +1,11 @@
 // Author: dWallet Labs, Ltd.
 // SPDX-License-Identifier: BSD-3-Clause-Clear
-
+//! Proof Aggregation Traits.
+//!
+//! Each state of the MPC protocol is represented by a different Rust
+//! type. The state transitions consume the previous state, making it
+//! a compile error to perform the steps out of order or to repeat a
+//! step.
 use std::collections::HashMap;
 
 use crypto_bigint::rand_core::CryptoRngCore;
@@ -35,10 +40,14 @@ pub type Result<T> = std::result::Result<T, Error>;
 
 /// The commitment round party of a proof aggregation protocol.
 pub trait CommitmentRoundParty<Output>: Sized {
+    /// The output of the commitment round.
     type Commitment: Serialize + for<'a> Deserialize<'a> + Clone;
 
+    /// The round following the commitment round.
     type DecommitmentRoundParty: DecommitmentRoundParty<Output, Commitment = Self::Commitment>;
 
+    /// The commitment round of the proof aggregation protocol.
+    /// Receives `self` and not `&self`, to enforce [`Self::DecommitmentRoundParty`] can be instantiated only via a valid state transition.
     fn commit_statements_and_statement_mask(
         self,
         rng: &mut impl CryptoRngCore,
@@ -47,10 +56,16 @@ pub trait CommitmentRoundParty<Output>: Sized {
 
 /// The decommitment round party of a proof aggregation protocol.
 pub trait DecommitmentRoundParty<Output>: Sized {
+    /// The output of the round preceding the decommitment round.
     type Commitment: Serialize + for<'a> Deserialize<'a> + Clone;
+    /// The output of the decommitment round.
     type Decommitment: Serialize + for<'a> Deserialize<'a> + Clone;
+    /// The round following the decommitment round.
     type ProofShareRoundParty: ProofShareRoundParty<Output, Decommitment = Self::Decommitment>;
 
+    /// The decommitment round of the proof aggregation protocol.
+    /// Receives `self` and not `&self`, to enforce [`Self::ProofShareRoundParty`] can be instantiated only via a valid state transition.
+    /// Receives `commitments` the outputs of the preceding round from all parties.
     fn decommit_statements_and_statement_mask(
         self,
         commitments: HashMap<PartyID, Self::Commitment>,
@@ -60,13 +75,19 @@ pub trait DecommitmentRoundParty<Output>: Sized {
 
 /// The proof share round party of a proof aggregation protocol.
 pub trait ProofShareRoundParty<Output>: Sized {
+    /// The output of the round preceding the proof share round.
     type Decommitment: Serialize + for<'a> Deserialize<'a> + Clone;
+    /// The output of the proof share round.
     type ProofShare: Serialize + for<'a> Deserialize<'a> + Clone;
+    /// The round following the proof share round.
     type ProofAggregationRoundParty: ProofAggregationRoundParty<
         Output,
         ProofShare = Self::ProofShare,
     >;
 
+    /// The decommitment round of the proof aggregation protocol.
+    /// Receives `self` and not `&self`, to enforce [`Self::ProofAggregationRoundParty`] can be instantiated only via a valid state transition.
+    /// Receives `decommitments` the outputs of the preceding round from all parties.
     fn generate_proof_share(
         self,
         decommitments: HashMap<PartyID, Self::Decommitment>,
@@ -76,8 +97,11 @@ pub trait ProofShareRoundParty<Output>: Sized {
 
 /// The proof aggregation round party of a proof aggregation protocol.
 pub trait ProofAggregationRoundParty<Output>: Sized {
+    /// The output of the round preceding the proof aggregation round.
     type ProofShare: Serialize + for<'a> Deserialize<'a> + Clone;
 
+    /// The finalization logic of the aggregation protocol.
+    /// Receives `proof_shares` the outputs of the preceding round from all parties.
     fn aggregate_proof_shares(
         self,
         proof_shares: HashMap<PartyID, Self::ProofShare>,
