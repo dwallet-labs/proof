672492532c79718d1cca9929fea1a957493f57fc
Yehonatan Cohen Scaly <scalys7@gmail.com>
2024-02-04 18:06:10 +0200

    Proof Aggregation Traits (#4)

---
+++ src/lib.rs
@@ -0,0 +1,32 @@
+// Author: dWallet Labs, LTD.
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+
+pub mod aggregation;
+
+/// Proof error.
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("invalid parameters")]
+    InvalidParameters,
+
+    #[error("an internal error that should never have happened and signifies a bug")]
+    InternalError,
+
+    #[error("aggregation error")]
+    Aggregation(#[from] aggregation::Error),
+
+    #[error("serialization/deserialization error")]
+    Serialization(#[from] serde_json::Error),
+
+    #[error("invalid proof - did not satisfy the verification equation")]
+    ProofVerification,
+
+    #[error("group error")]
+    GroupInstantiation(#[from] group::Error),
+
+    #[error("at least one of the witnesses is out of range")]
+    OutOfRange,
+}
+
+/// Proof result.
+pub type Result<T> = std::result::Result<T, Error>;
