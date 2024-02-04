1b8d4e3f5d5479163301e9e34b07dc0fccb33f59
Yehonatan Cohen Scaly <scalys7@gmail.com>
2024-02-04 13:43:59 +0200

    fixed test now??

--- src/aggregation.rs
+++ src/aggregation.rs
@@ -277,10 +277,15 @@ pub mod test_helpers {
             proof_aggregation_parties.into_iter().all(|(party_id, proof_aggregation_round_party)| {
                 let unresponsive_parties_for_party: Vec<_> = unresponsive_parties.clone().into_iter().filter(|&pid| pid != party_id).collect();
 
-                matches!(
-                    proof_aggregation_round_party.aggregate_proof_shares(filtered_proof_shares.clone(), &mut OsRng).err().unwrap().try_into().unwrap(),
-                    Error::UnresponsiveParties(parties) if parties == unresponsive_parties_for_party
-                )
+                let res = proof_aggregation_round_party.aggregate_proof_shares(filtered_proof_shares.clone(), &mut OsRng);
+                if unresponsive_parties_for_party.is_empty() {
+                    res.is_ok()
+                } else {
+                    matches!(
+                        res.err().unwrap().try_into().unwrap(),
+                        Error::UnresponsiveParties(parties) if parties == unresponsive_parties_for_party
+                    )
+                }
             }
             )
         );
